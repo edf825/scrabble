c7617f2ae748f56d7b094965af356e2929310ea9
Chris Flores <chris@flores.geek.nz>
2012-12-26 16:20:31 +1300

    Issue #1, Displayed game board as table; fixed front page link.

--- frontpage.js
+++ frontpage.js
@@ -2,7 +2,7 @@ function handleRequest(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     
     console.log('test');
-    res.write("<a href=\"/newboard\">New Board</a>");
+    res.write("<a href=\"/newgame\">Click for new game</a>");
     res.end();
 }
 
--- game.js
+++ game.js
@@ -24,6 +24,30 @@ function handleCreateGame(req, res) {
 }
 
 function handleDisplayBoard(req, res) {
+    console.log('displaying board');
+    
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    var urlBits = req.url.split('/');
+    
+    var gameID = parseInt(urlBits[2]);
+    var game = games[gameID];
+    
+    game.board[0][0] = "A";
+    game.board[0][13] = "B";
+    game.board[4][6] = "C";
+    
+    res.write("<table border='2' id='board'>");
+    for (var i = 0; i < 15; i++) {
+        res.write("<tr>");
+        for (var j = 0; j < 15; j++){
+            res.write("<td height='50' width='50'>" + game.board[i][j] + "</td>");
+        }
+        res.write("</tr>");
+    }
+    res.write("</table>");    
+    
+    res.end();
 }
 
 function handleDisplayRack(req, res) {
