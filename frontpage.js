3126a2111582e5d00e3e134d2e858680fb51d0c1
Chris Flores <chris@flores.geek.nz>
2012-12-26 15:51:18 +1300

    frontpage change

--- frontpage.js
+++ frontpage.js
@@ -1,4 +1,9 @@
 function handleRequest(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    console.log('test');
+    res.write("<a href=\"/newboard\">New Board</a>");
+    res.end();
 }
 
 exports.handleRequest = handleRequest;
