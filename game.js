bb5b801714a5108f951580475864661396343fc5
Chris Flores <chris@flores.geek.nz>
2012-12-26 17:04:46 +1300

    used CSS to format table instead of html properties

--- game.js
+++ game.js
@@ -28,6 +28,25 @@ function handleDisplayBoard(req, res) {
     
     res.writeHead(200, {'Content-Type': 'text/html'});
     
+    //CSS
+    res.write("<style type='text/css'>" +
+    "table#board{" +
+    "border: 5px solid black;" +
+    "}" +
+    "td.boardtile{" +
+    "border: 2px solid black;" +
+    "height: 50px;" +
+    "width: 50px;" +
+    "vertical-align: center;" +
+    "text-align: center;" +
+    "}" +
+    "p.tiletext{" + 
+    "font-family: serif;" + 
+    "font-size: 40px;" +
+    "font-weight: 700;" + 
+    "}" +
+    "</style>");
+    
     var urlBits = req.url.split('/');
     
     var gameID = parseInt(urlBits[2]);
@@ -37,11 +56,12 @@ function handleDisplayBoard(req, res) {
     game.board[0][13] = "B";
     game.board[4][6] = "C";
     
-    res.write("<table border='2' id='board'>");
+    res.write("<table id='board'>");
     for (var i = 0; i < 15; i++) {
         res.write("<tr>");
         for (var j = 0; j < 15; j++){
-            res.write("<td height='50' width='50'>" + game.board[i][j] + "</td>");
+            res.write("<td class='boardtile'><p id='tiletext'>" + 
+            game.board[i][j] + "</p></td>");
         }
         res.write("</tr>");
     }
