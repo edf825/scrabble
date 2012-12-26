36d29099cae6434d506d11c3ab61cc44f29c0faa
Edwin Flores <eflores@mozilla.com>
2012-12-26 15:46:28 +1300

    Init board

--- game.js
+++ game.js
@@ -11,6 +11,14 @@ function handleCreateGame(req, res) {
 
   var game = games[gameId] = {};
 
+  game.board = [];
+  for (var i = 0; i < 15; i++) {
+    game.board[i] = [];
+    for (var j = 0; j < 15; j++) {
+      game.board[i][j] = ' ';
+    }
+  }
+
   res.writeHead(302, { 'Location' : '/board/' + gameId });
   res.end();
 }
