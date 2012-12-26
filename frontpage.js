c9fb352fdc53c1685610a4f5fd0169acae95f5b3
Edwin Flores <eflores@mozilla.com>
2012-12-26 15:00:41 +1300

    Branch on requested URL

---
+++ frontpage.js
@@ -0,0 +1,4 @@
+function handleRequest(req, res) {
+}
+
+exports.handleRequest = handleRequest;
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var http = require('http');
 
 function handleRequest(req, res)
 {
-  res.writeHead(200, { 'Content-Type' : 'text/html' });
   console.log('Requested URL: ' + req.url);
-  res.end('OK!');
+  if (req.url == '/') {
+    require('./frontpage.js').handleRequest(req, res);
+  } else {
+    res.writeHead(200, { 'Content-Type' : 'text/html' });
+    res.end('OK!');
+  }
 }
 
 http.createServer(handleRequest).listen(8080);
