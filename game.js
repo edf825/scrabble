122ca47f3d39462638aadff89d340f580a726fbf
Edwin Flores <eflores@mozilla.com>
2012-12-26 15:26:41 +1300

    Added game.js

---
+++ game.js
@@ -0,0 +1,20 @@
+var boards = [];
+var lastBoardId = 0;
+
+function init() {
+}
+
+function handleCreateBoard(req, res) {
+  var boardId = ++lastBoardId;
+
+  // Board init stuff
+
+  res.writeHead(302, { 'Location' : '/board/' + boardId });
+  res.end();
+}
+
+function handleDisplayBoard(req, res) {
+}
+
+function handleDisplayTiles(req, res) {
+}
